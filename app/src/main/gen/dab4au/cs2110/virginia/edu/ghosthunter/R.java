9219679074065bb4d4c7ef42a413d9af072919e8
Leah E. Procopi <leah.procopi@gmail.com>
2015-03-30 18:15:51 -0400

    Leah testing first commit

---
+++ app/src/main/gen/dab4au/cs2110/virginia/edu/ghosthunter/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package dab4au.cs2110.virginia.edu.ghosthunter;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
